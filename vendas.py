517d6399a1cd2b50996a9f9990070a50929251c0
Lucas Aragão <lucas.magno@dce.ufpb.br>
2016-05-24 14:48:03 -0300

    Update vendas.py

--- vendas.py
+++ vendas.py
@@ -1,18 +1,20 @@
 def menu():
-    print("1  - INSERIR NOVO FUNCIONARIO")
-    print("2  - INSERIR MEDICAMENTOS")
-    print("3  - VERIFICAR SE ESSE MEDICAMENTO NECESSITA DE RECEITA")
-    print("4  - ALTERAR STATUS DE RECEITA DO MEDICAMENTO")
-    print("5  - VERIFICAR ESTOQUE DO MEDICAMENTO")
-    print("6  - ATUALIZAR ESTOQUE")
-    print("7  - REALIZAR NOVA VENDA")
-    print("8  - VENDEDOR QUE MAIS REALIZOU VENDAS")
+    print("1 - INSERIR NOVO FUNCIONARIO")
+    print("2 - INSERIR MEDICAMENTOS")
+    print("3 - VERIFICAR SE ESSE MEDICAMENTO NECESSITA DE RECEITA")
+    print("4 - ALTERAR STATUS DE RECEITA DO MEDICAMENTO")
+    print("5 - VERIFICAR ESTOQUE DO MEDICAMENTO")
+    print("6 - ATUALIZAR ESTOQUE")
+    print("7 - REALIZAR NOVA VENDA")
+    print("8 - VENDEDOR QUE MAIS REALIZOU VENDAS")
     print("10 - SAIR")
     op=input("DIGITE SUA OPÇÃO: \n")
     return op
 
+(menu())
+#-------------------------------------------------------------------
 def add_func():
-    arq= open("Cdastro.txt", "a")
+    arq= open("Cadastro.txt", "a")
     funcionario=input("DIGITE SEU NOME: ")
     codigo=int(input("DIGITE SEU CODIGO: "))
     print(funcionario, file = arq)
@@ -20,3 +22,14 @@ def add_func():
     arq.close()
 
 (add_func())
+
+#----------------------------------------------------------------------
+def leitura_func():
+    arq = open("Cadastro.txt", "r")
+    funcionario = arq.read()
+    codigo = arq.read()
+    print(funcionario)
+    print(codigo)
+    arq.close()
+
+    
